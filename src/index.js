ee3f8bfbf6baae59382b6caf2e3c1360dc69edc5
Rytis Alekna <ralekna@exadel.com>
2017-12-02 12:46:27 +0200

    better indexing

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 const request = require('request-promise-native');
 const printf = require('printf');
 
-const colors = {
-  'green': ''
-};
+/**
+ * TODO:
+ * coloring
+ * available markets
+ * available exchange currencies (pairs)
+ * google results
+ * volatility index
+ */
 
 (async () => {
     let allData;
@@ -15,7 +20,9 @@ const colors = {
         console.error(`An error occurred while retrieving data ${error}`);
         return;
     }
-    
+
+    console.log(`Retrieved ${allData.length} currencies`);
+
     const bitCoinData = allData.find(item => item.symbol === 'BTC');
 
     const btcSupply = parseFloat(bitCoinData['available_supply']);
@@ -28,8 +35,16 @@ const colors = {
     console.log(`BitCoin stats`);
     console.log(`Price USD: ${bitCoinData.price_usd} supply: ${bitCoinData.available_supply} volume 24 USD: ${bitCoinData['24h_volume_usd']}\n`);
 
-    console.log(`    Symbol |               Name |  price USD |         supply |  volume 24 USD`);
-    console.log(`------------------------------------------------------------------------------`);
+    console.log(`
+        Index fractions meaning:
+        S  - supply: (btcTotalSupply / coinTotalSupply)
+        P  - price: (1 - coinPrice/btcPrice)
+        V  - volume: (coinVolumeUsd / coinCapitalizationUsd)
+        VB - BTC volume: (coin24HoursUsdVolume / btc24HoursUsdVolume)
+    `);
+
+    console.log(`No.  |  Symbol |                         Name |   price USD |         supply |  volume 24 USD |  CMC rank | Index fractions`);
+    console.log(`------------------------------------------------------------------------------------------------------------------------------------------`);
 
     let maxSupplyRatio;
     let maxPriceRatio;
@@ -40,18 +55,20 @@ const colors = {
         let price = parseFloat(data['price_usd']);
         let supply = parseFloat(data['available_supply']);
         let volume = parseFloat(data['24h_volume_usd']);
+        let rank = parseInt(data['rank']);
 
         let supplyRatio = btcSupply / supply;
-        let priceRatio = (1 - parseFloat(data['price_btc']));
+        let priceRatio = (1 - parseFloat(data['price_btc'])) + Number.EPSILON;
         let volumeRatio = volume / parseFloat(data['market_cap_usd']);
         let btcVolumeRatio = parseFloat(data['24h_volume_usd']) / btcVolume;
+        let rankRatio = Math.sqrt(1 / rank);
 
         supplyRatio = isNaN(supplyRatio) ? 0 : supplyRatio;
         priceRatio = isNaN(priceRatio) ? 0 : priceRatio;
         volumeRatio = isNaN(volumeRatio) ? 0 : volumeRatio;
         btcVolumeRatio = isNaN(btcVolumeRatio) ? 0 : btcVolumeRatio;
 
-        let index = supplyRatio * priceRatio * volumeRatio * btcVolumeRatio;
+        let index = supplyRatio * priceRatio * volumeRatio * btcVolumeRatio * rankRatio;
 
         return {
             symbol: data.symbol,
@@ -64,18 +81,19 @@ const colors = {
             btcVolumeRatio,
             price,
             supply,
-            volume
+            volume,
+            rank
         };
     })
     .sort((a, b) => b.index - a.index);
 
     processedData
-    .filter(coin => (coin.supply > 1000000 && coin.volume > 1000000 &&  coin.price < btcPrice))
-    .slice(0, 50)
-    .forEach(coin => {
-        console.log(`${printf('%10s', coin.symbol)} ${printf('%20s', coin.name)}    ${printf('%10s', coin.price.toFixed(5))}   ${printf('%14s', parseInt(coin.data.available_supply))} ${printf('%16s', parseInt(coin.data['24h_volume_usd']))}| S: ${coin.supplyRatio.toFixed(5)} | P: ${coin.priceRatio.toFixed(5)} | V: ${coin.volumeRatio.toFixed(5)} | VB: ${coin.btcVolumeRatio.toFixed(5)}`);
+    .filter(coin => (coin.supply > 1000000 && coin.volume > 1000000 &&  coin.price <= btcPrice))
+    // .slice(0, 2000)
+    .forEach((coin, index) => {
+        console.log(`${printf('%4d', index + 1)}  ${printf('%8s', coin.symbol)} ${printf('%30s', coin.name)}  ${printf('%12s', coin.price.toFixed(5))}   ${printf('%14s', parseInt(coin.data.available_supply))} ${printf('%16s', parseInt(coin.data['24h_volume_usd']))}        ${printf('%4s',coin.rank)} | S: ${coin.supplyRatio.toPrecision(10)} | P: ${coin.priceRatio.toPrecision(10)} | V: ${coin.volumeRatio.toPrecision(10)} | VB: ${coin.btcVolumeRatio.toPrecision(10)}`);
     });
-    console.log(`------------------------------------------------------------------------------\n`);
+    console.log(`------------------------------------------------------------------------------------------------------------------------------------------`);
 
 
 })();
