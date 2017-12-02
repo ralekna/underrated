eadb39f157c9f8ba8001a595ad5e38197e94e873
Rytis Alekna <ralekna@exadel.com>
2017-12-02 04:53:18 +0200

    sanity filtering

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 const request = require('request-promise-native');
 const printf = require('printf');
 
+const colors = {
+  'green': ''
+};
+
 (async () => {
     let allData;
 
@@ -16,48 +20,64 @@ const printf = require('printf');
 
     const btcSupply = parseFloat(bitCoinData['available_supply']);
     const btcVolume = parseFloat(bitCoinData['24h_volume_usd']);
+    const btcPrice = parseFloat(bitCoinData['price_usd']);
 
 
-    // btcSup/sup + (1-price/btcPrice) + volume/btcVolume
-    // btcSup/sup + (1-price/btcPrice) + volume_usd/cap_usd
+    // btcTotalSupply / coinTotalSupply * (1 - coinPrice/btcPrice) * (coinVolumeUsd / coinCapitalizationUsd) * (coin24HoursUsdVolume / btc24HoursUsdVolume)
 
     console.log(`BitCoin stats`);
     console.log(`Price USD: ${bitCoinData.price_usd} supply: ${bitCoinData.available_supply} volume 24 USD: ${bitCoinData['24h_volume_usd']}\n`);
 
-    console.log(`    Symbol |               Name |       Index  |  price USD |         supply |  volume 24 USD`);
-    console.log(`---------------------------------------------------------------------------------------------`);
+    console.log(`    Symbol |               Name |  price USD |         supply |  volume 24 USD`);
+    console.log(`------------------------------------------------------------------------------`);
 
+    let maxSupplyRatio;
+    let maxPriceRatio;
+    let maxVolumeRatio;
+    let maxBtcVolumeRatio;
 
-
-    allData.map(data => {
+    let processedData = allData.map(data => {
         let price = parseFloat(data['price_usd']);
+        let supply = parseFloat(data['available_supply']);
+        let volume = parseFloat(data['24h_volume_usd']);
 
-        let suplyRatio = btcSupply/parseFloat(data['available_supply']);
+        let supplyRatio = btcSupply / supply;
         let priceRatio = (1 - parseFloat(data['price_btc']));
-        let volumeRatio = parseFloat(data['24h_volume_usd']) / parseFloat(data['market_cap_usd']);
+        let volumeRatio = volume / parseFloat(data['market_cap_usd']);
         let btcVolumeRatio = parseFloat(data['24h_volume_usd']) / btcVolume;
 
-        suplyRatio = isNaN(suplyRatio) ? 0 : suplyRatio;
+        supplyRatio = isNaN(supplyRatio) ? 0 : supplyRatio;
         priceRatio = isNaN(priceRatio) ? 0 : priceRatio;
         volumeRatio = isNaN(volumeRatio) ? 0 : volumeRatio;
         btcVolumeRatio = isNaN(btcVolumeRatio) ? 0 : btcVolumeRatio;
 
-        let index = (suplyRatio * priceRatio) * volumeRatio * btcVolumeRatio;
+        let index = supplyRatio * priceRatio * volumeRatio * btcVolumeRatio;
 
         return {
-            price: price.toFixed(5),
             symbol: data.symbol,
             name: data.name,
             index,
-            data
+            data,
+            supplyRatio: supplyRatio,
+            priceRatio,
+            volumeRatio,
+            btcVolumeRatio,
+            price,
+            supply,
+            volume
         };
     })
-    .sort((a, b) => b.index - a.index)
-    .slice(0, 30)
+    .sort((a, b) => b.index - a.index);
+
+    processedData
+    .filter(coin => (coin.supply > 1000000 && coin.volume > 1000000 &&  coin.price < btcPrice))
+    .slice(0, 50)
     .forEach(coin => {
-        console.log(`${printf('%10s', coin.symbol)} ${printf('%20s', coin.name)}    ${printf('%10s', coin.index.toFixed(3))}    ${printf('%10s', coin.price)}   ${printf('%14s', parseInt(coin.data.available_supply))} ${printf('%16s', parseInt(coin.data['24h_volume_usd']))}`);
+        console.log(`${printf('%10s', coin.symbol)} ${printf('%20s', coin.name)}    ${printf('%10s', coin.price.toFixed(5))}   ${printf('%14s', parseInt(coin.data.available_supply))} ${printf('%16s', parseInt(coin.data['24h_volume_usd']))}| S: ${coin.supplyRatio.toFixed(5)} | P: ${coin.priceRatio.toFixed(5)} | V: ${coin.volumeRatio.toFixed(5)} | VB: ${coin.btcVolumeRatio.toFixed(5)}`);
     });
-    console.log(`------------------------------------------------------------------------------------------`);
+    console.log(`------------------------------------------------------------------------------\n`);
+
+
 })();
 
 
